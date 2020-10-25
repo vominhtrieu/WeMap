59fe7aa90055c2dc4e96c9f67e12ec380a33a416
vominhtrieu <minhtrieuvo600@gmail.com>
2020-10-25 17:22:35 +0700

    Smooth animation for location indicator movement and proper behavior for camera

--- app/src/main/java/hcmus/student/map/MapsActivity.java
+++ app/src/main/java/hcmus/student/map/MapsActivity.java
@@ -1,16 +1,24 @@
 package hcmus.student.map;
 
 import android.Manifest;
+import android.animation.ValueAnimator;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -28,8 +36,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -39,21 +49,24 @@ import java.util.Collections;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int LOCATION_STATUS_CODE = 1;
-    private static final long UPDATE_INTERVAL = 500;
-    private static final long FASTEST_UPDATE_INTERVAL = 100;
+    private static final int DEFAULT_ZOOM = 15;
+    private static final long UPDATE_INTERVAL = 1000;
+    private static final long FASTEST_UPDATE_INTERVAL = 1000;
+    private static final long ANIMATION_DURATION = 1000;
+    private static final double FOLLOWING_THRESHOLD = 0.00001;
 
     private GoogleMap mMap;
     private Location mCurrentLocation;
-    private FusedLocationProviderClient client;
+    private FusedLocationProviderClient mClient;
+    private Marker mLocationIndicator;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
-        client = LocationServices.getFusedLocationProviderClient(this);
+        mClient = LocationServices.getFusedLocationProviderClient(this);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -62,7 +75,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             showAlert();
         } else {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_STATUS_CODE);
-            enableLocation();
         }
     }
 
@@ -78,9 +90,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation.getLatitude(),
-                mCurrentLocation.getLongitude()), 15));
 
+        ImageButton btnLocation = findViewById(R.id.btnLocation);
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), DEFAULT_ZOOM
+                ));
+            }
+        });
+
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) getResources().getDrawable(R.drawable.location_indicator);
+        Bitmap bitmap = Bitmap.createScaledBitmap(bitmapDrawable.getBitmap(), 48, 48, false);
+        bitmapDrawable.setAntiAlias(true);
+        mLocationIndicator = mMap.addMarker(new MarkerOptions().position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
+                .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation.getLatitude(),
+                mCurrentLocation.getLongitude()), DEFAULT_ZOOM));
+
+        //Request change location setting
         final LocationRequest request = LocationRequest.create();
         request.setInterval(UPDATE_INTERVAL);
         request.setFastestInterval(FASTEST_UPDATE_INTERVAL);
@@ -95,20 +125,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
                 if (ActivityCompat.checkSelfPermission(MapsActivity.this,
                         Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    mMap.setMyLocationEnabled(true);
-                    client.requestLocationUpdates(request, new LocationCallback() {
+                    mClient.requestLocationUpdates(request, new LocationCallback() {
                         @Override
                         public void onLocationResult(LocationResult locationResult) {
                             super.onLocationResult(locationResult);
                             mCurrentLocation = locationResult.getLastLocation();
-
-                            //Camera won't move to user location if user location is out of screen bounds
-                            LatLngBounds screenBounds = mMap.getProjection().getVisibleRegion().latLngBounds;
-                            if (mCurrentLocation != null &&
-                                    screenBounds.contains(new LatLng(mCurrentLocation.getLatitude(),
-                                    mCurrentLocation.getLongitude()))) {
-                                mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(mCurrentLocation.getLatitude(),
-                                        mCurrentLocation.getLongitude())));
+                            
+                            if (mCurrentLocation != null) {
+                                animateLocationIndicator();
                             }
                         }
                     }, Looper.myLooper());
@@ -118,24 +142,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_STATUS_CODE:
+                enableLocation();
+                break;
         }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(false);
-        }
+    private void animateLocationIndicator() {
+        ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
+
+        final LatLng startLatLng = mLocationIndicator.getPosition();
+        final LatLng endLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+
+        animator.setDuration(ANIMATION_DURATION);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fraction = animation.getAnimatedFraction();
+                double newLat = startLatLng.latitude + (endLatLng.latitude - startLatLng.latitude) * fraction;
+                double newLng = startLatLng.longitude + (mCurrentLocation.getLongitude() - startLatLng.longitude) * fraction;
+                LatLng newPosition = new LatLng(newLat, newLng);
+
+                //Check if user moved camera, if yes, camera won't follow user's location
+                LatLng cameraTargetPosition = mMap.getCameraPosition().target;
+                LatLng indicatorPosition = mLocationIndicator.getPosition();
+
+                double latitudeDiff = cameraTargetPosition.latitude - indicatorPosition.latitude;
+                double longitudeDiff = cameraTargetPosition.longitude - indicatorPosition.longitude;
+
+                //We don't compute square root because this result is enough for checking whether user move the camera
+                double squareDistance = latitudeDiff * latitudeDiff + longitudeDiff * longitudeDiff;
+
+                if (squareDistance <= FOLLOWING_THRESHOLD * FOLLOWING_THRESHOLD)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(newPosition));
+                mLocationIndicator.setPosition(newPosition);
+            }
+        });
+        animator.start();
     }
+
 
     private void enableLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Task<Location> task = client.getLastLocation();
+            Task<Location> task = mClient.getLastLocation();
             task.addOnSuccessListener(new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
@@ -147,6 +200,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
             });
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+            builder.setTitle("Warning!");
+            builder.setMessage("You have denied the app to access your permission, this app won't work");
+            builder.setCancelable(false);
+            builder.setPositiveButton("Yes, I know", new DialogInterface.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            showAlert();
         }
     }
 
@@ -160,7 +229,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_STATUS_CODE);
-                enableLocation();
             }
         });
         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
