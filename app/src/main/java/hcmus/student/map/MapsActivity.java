87707c1a55144c253a1522c7a670cb8d29496e35
vominhtrieu <minhtrieuvo600@gmail.com>
2020-11-02 08:31:46 +0700

    Change logo and icon

--- app/src/main/java/hcmus/student/map/MapsActivity.java
+++ app/src/main/java/hcmus/student/map/MapsActivity.java
@@ -11,17 +11,15 @@ import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -38,7 +36,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -52,13 +49,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int DEFAULT_ZOOM = 15;
     private static final long UPDATE_INTERVAL = 1000;
     private static final long FASTEST_UPDATE_INTERVAL = 1000;
-    private static final long ANIMATION_DURATION = 1000;
-    private static final double FOLLOWING_THRESHOLD = 0.00001;
+    private static final long ANIMATION_DURATION = 500;
+    private static final double FOLLOWING_THRESHOLD = 0.00000001;
 
     private GoogleMap mMap;
     private Location mCurrentLocation;
     private FusedLocationProviderClient mClient;
     private Marker mLocationIndicator;
+    private LocationCallback mLocationCallBack;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -67,7 +65,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
         mClient = LocationServices.getFusedLocationProviderClient(this);
-
+        mLocationCallBack = null;
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             enableLocation();
@@ -78,67 +76,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    protected void onResume() {
+        super.onResume();
+        listenToLocationChange();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mLocationCallBack != null) {
+            mClient.removeLocationUpdates(mLocationCallBack);
+        }
+    }
+
+
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
-
         ImageButton btnLocation = findViewById(R.id.btnLocation);
+
         btnLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                        new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), DEFAULT_ZOOM
+                        new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()),
+                        mMap.getCameraPosition().zoom >= DEFAULT_ZOOM ? mMap.getCameraPosition().zoom : DEFAULT_ZOOM
                 ));
             }
         });
 
-        BitmapDrawable bitmapDrawable = (BitmapDrawable) getResources().getDrawable(R.drawable.location_indicator);
-        Bitmap bitmap = Bitmap.createScaledBitmap(bitmapDrawable.getBitmap(), 48, 48, false);
+        //Display location indicator
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.location_indicator,
+                getTheme());
+        Bitmap bitmap = Bitmap.createScaledBitmap(bitmapDrawable.getBitmap(), 72, 72, false);
         bitmapDrawable.setAntiAlias(true);
         mLocationIndicator = mMap.addMarker(new MarkerOptions().position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
                 .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
 
+        //Move camera to user location with default zoom
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation.getLatitude(),
                 mCurrentLocation.getLongitude()), DEFAULT_ZOOM));
 
-        //Request change location setting
-        final LocationRequest request = LocationRequest.create();
-        request.setInterval(UPDATE_INTERVAL);
-        request.setFastestInterval(FASTEST_UPDATE_INTERVAL);
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addAllLocationRequests(Collections.singleton(request));
-        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
-        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
-
-        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
-            @Override
-            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                if (ActivityCompat.checkSelfPermission(MapsActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    mClient.requestLocationUpdates(request, new LocationCallback() {
-                        @Override
-                        public void onLocationResult(LocationResult locationResult) {
-                            super.onLocationResult(locationResult);
-                            mCurrentLocation = locationResult.getLastLocation();
-                            
-                            if (mCurrentLocation != null) {
-                                animateLocationIndicator();
-                            }
-                        }
-                    }, Looper.myLooper());
-                }
-            }
-        });
+        listenToLocationChange();
     }
 
     @Override
@@ -146,15 +125,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case LOCATION_STATUS_CODE:
-                enableLocation();
+                if (grantResults.length > 0)
+                    enableLocation();
                 break;
         }
     }
 
     private void animateLocationIndicator() {
-        ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
+        final ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
 
         final LatLng startLatLng = mLocationIndicator.getPosition();
+        final Location tempLocation = mCurrentLocation;
         final LatLng endLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
 
         animator.setDuration(ANIMATION_DURATION);
@@ -162,9 +143,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
+                if (mCurrentLocation != tempLocation) {
+                    animator.cancel();
+                    return;
+                }
                 float fraction = animation.getAnimatedFraction();
                 double newLat = startLatLng.latitude + (endLatLng.latitude - startLatLng.latitude) * fraction;
-                double newLng = startLatLng.longitude + (mCurrentLocation.getLongitude() - startLatLng.longitude) * fraction;
+                double newLng = startLatLng.longitude + (endLatLng.longitude - startLatLng.longitude) * fraction;
                 LatLng newPosition = new LatLng(newLat, newLng);
 
                 //Check if user moved camera, if yes, camera won't follow user's location
@@ -174,17 +159,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 double latitudeDiff = cameraTargetPosition.latitude - indicatorPosition.latitude;
                 double longitudeDiff = cameraTargetPosition.longitude - indicatorPosition.longitude;
 
-                //We don't compute square root because this result is enough for checking whether user move the camera
+                //We don't compute square root because this result is enough for checking whether user move the camera or not
                 double squareDistance = latitudeDiff * latitudeDiff + longitudeDiff * longitudeDiff;
 
-                if (squareDistance <= FOLLOWING_THRESHOLD * FOLLOWING_THRESHOLD)
+                if (squareDistance < FOLLOWING_THRESHOLD)
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(newPosition));
                 mLocationIndicator.setPosition(newPosition);
             }
         });
         animator.start();
     }
-
 
     private void enableLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -205,7 +189,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             builder.setTitle("Warning!");
             builder.setMessage("You have denied the app to access your permission, this app won't work");
             builder.setCancelable(false);
-            builder.setPositiveButton("Yes, I know", new DialogInterface.OnClickListener() {
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                 @RequiresApi(api = Build.VERSION_CODES.M)
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -215,8 +199,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             AlertDialog dialog = builder.create();
             dialog.show();
-            showAlert();
         }
+    }
+
+    private void listenToLocationChange() {
+        //Request change location setting
+        final LocationRequest request = LocationRequest.create();
+        request.setInterval(UPDATE_INTERVAL);
+        request.setFastestInterval(FASTEST_UPDATE_INTERVAL);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addAllLocationRequests(Collections.singleton(request));
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
+
+        //Listen for location change event
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                if (ActivityCompat.checkSelfPermission(MapsActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                    mLocationCallBack = new LocationCallback() {
+                        @Override
+                        public void onLocationResult(LocationResult locationResult) {
+                            super.onLocationResult(locationResult);
+                            mCurrentLocation = locationResult.getLastLocation();
+
+                            if (mCurrentLocation != null) {
+                                animateLocationIndicator();
+                            }
+                        }
+                    };
+
+                    mClient.requestLocationUpdates(request, mLocationCallBack, Looper.myLooper());
+                }
+            }
+        });
     }
 
     private void showAlert() {
@@ -234,7 +253,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                finish();
+                dialog.dismiss();
             }
         });
 
