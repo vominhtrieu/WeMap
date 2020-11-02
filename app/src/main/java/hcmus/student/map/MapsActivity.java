993d9bf0d3cb1076810f331dfc0850b62d5ba8a2
vominhtrieu <minhtrieuvo600@gmail.com>
2020-11-02 10:56:35 +0700

    Fix mistake when merging

--- app/src/main/java/hcmus/student/map/MapsActivity.java
+++ app/src/main/java/hcmus/student/map/MapsActivity.java
@@ -62,7 +62,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private FusedLocationProviderClient mClient;
     private Marker mLocationIndicator;
     private LocationCallback mLocationCallBack;
-    Marker marker;
+    private Marker marker;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -78,22 +78,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.d("Azimuth", Float.toString(rotation));
             }
         };
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
 
         mClient = LocationServices.getFusedLocationProviderClient(this);
         mLocationCallBack = null;
@@ -122,7 +106,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
