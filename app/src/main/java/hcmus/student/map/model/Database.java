d50e4c64e903e363714b7a358184db167e9763fd
vominhtrieu <minhtrieuvo600@gmail.com>
2020-12-03 22:11:24 +0700

    Fixed create table statement

--- app/src/main/java/hcmus/student/map/model/Database.java
+++ app/src/main/java/hcmus/student/map/model/Database.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -13,7 +15,6 @@ import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class Database extends SQLiteOpenHelper {
     static final String DATABASE_NAME = "Contact List";
@@ -26,8 +27,11 @@ public class Database extends SQLiteOpenHelper {
     static final String KEY_FAVORITE = "FAVORITE";
     static final int DATABASE_VERSION = 1;
 
+    private Context context;
+
     public Database(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     private List<Place> getPlacesFromCursor(Cursor cursor) {
@@ -108,10 +112,10 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = String.format(Locale.US, "CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        "%s TEXT, %s REAL UNIQUE, %s REAL UNIQUE, %s BLOB, %s TEXT))",
+        String sql = String.format(
+                "CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s REAL, %s REAL, %s BLOB, %s TEXT)",
                 TABLE_NAME, KEY_ID, KEY_NAME, KEY_LATITUDE, KEY_LONGITUDE, KEY_AVATAR, KEY_FAVORITE);
-
+        Log.e("sql", sql);
         SQLiteStatement statement = db.compileStatement(sql);
         statement.execute();
     }
